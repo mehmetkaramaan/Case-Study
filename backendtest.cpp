b246a61dba419e2e69fd5488c9cc1a2f96ce3fdc
Mehmet Mikayil Karaman <mkaraman48@icloud.com>
2023-08-29 18:10:09 +0300

    Update backendtest.cpp

--- backendtest.cpp
+++ backendtest.cpp
@@ -1,53 +1,87 @@
 #include <iostream>
 #include <curl/curl.h>
-#include <string>
+#include <nlohmann/json.hpp>
 
-// Callback fonksiyonu verileri yazdýrýr
+using json = nlohmann::json; //using json = nlohmann::json; ifadesi, nlohmann/json kütüphanesinde tanýmlanan json tipini kendi kodumuzda daha kýsa bir isimle kullanabiemizi saðlayan bir tür "aliastýr" veya "typedef"dir.
+
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
 int main() {
-    // API URL
-    std::string url = "https://flights-api.buraky.workers.dev/";
+    CURL* curl;
+    CURLcode res;
+    std::string response;
 
-    // HTTP client baþlat
-    CURL* curl = curl_easy_init();
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
     if (curl) {
-     
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
-
         
-        std::string response_data;
+        curl_easy_setopt(curl, CURLOPT_URL, "https://flights-api.example.com/flights");
+
+      
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+       
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
         
-        CURLcode res = curl_easy_perform(curl);
+        res = curl_easy_perform(curl);
+
+      
         if (res != CURLE_OK) {
             std::cerr << "Curl error: " << curl_easy_strerror(res) << std::endl;
         } else {
             
-            long http_status_code = 0;
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_status_code);
+            long http_code = 0;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
-            // HTTP status code kontrolü
-            if (http_status_code != 200) {
-                std::cerr << "HTTP status code: " << http_status_code << std::endl;
-            } else {
-                // Response içeriði kontrolü
-                std::cout << "Response content:\n" << response_data << std::endl;
+            
+            if (http_code == 200) {
+              
+                try {
+                    json jsonResponse = json::parse(response);
+
+                    // Check response structure
+                    if (jsonResponse.contains("data") && jsonResponse["data"].is_array()) {
+                        for (const auto& flight : jsonResponse["data"]) {
+                            if (!flight.contains("id") || !flight.contains("from") || !flight.contains("to") || !flight.contains("date")) {
+                                std::cerr << "Invalid response format!" << std::endl;
+                                break;
+                            }
+                        }
+                    } else {
+                        std::cerr << "Invalid response format!" << std::endl;
+                    }
+                } catch (const std::exception& e) {
+                    std::cerr << "Error parsing JSON response: " << e.what() << std::endl;
+                }
 
                 
+                char* content_type;
+                res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &content_type);
+                if ((res == CURLE_OK) && content_type) {
+                    if (std::string(content_type) == "application/json") {
+                        std::cout << "Content-Type header is correct." << std::endl;
+                    } else {
+                        std::cerr << "Invalid Content-Type header!" << std::endl;
+                    }
+                }
+            } else {
+                std::cerr << "HTTP Error: " << http_code << std::endl;
             }
         }
 
-        // HTTP client temizle
+        
         curl_easy_cleanup(curl);
     }
 
+    
+    curl_global_cleanup();
+
     return 0;
 }
-// Bu C++ programýnda, belirtilen API'yi test etmek için libcurl kütüphanesini kullandým.
+
